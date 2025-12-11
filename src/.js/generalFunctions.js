32b1802220b9f4682549d826fdbf85fba48d539c
Michael <100490812+DebSol11@users.noreply.github.com>
2025-12-11 21:22:19 +0400

    work, than push -- push more often in the future

--- src/.js/generalFunctions.js
+++
@@ -1,1 +0,0 @@
-export const module1 = "Script module linkage example.";
--- src/.js/script.js
+++ src/.js/script.js
@@ -1,4 +1,42 @@
 import "../styles.css";
-import { module1 } from "./generalFunctions.js";
 
-console.log(module1);
+let inputField = document.querySelector("#citySelect");
+let searchBtn = document.querySelector("#weatherSearchBtn");
+let selectedCity = document.querySelector("#selectedCity");
+let currentTemperature = document.querySelector("currentTemperature");
+
+// API Url creation variables
+let baseUrlStart =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+let dynamicPartUrl = "Dubai";
+let baseUrlEnd =
+  "?unitGroup=us&include=days%2Ccurrent&key=SPQCWP3UPXJYLXTRTVL652LC5&contentType=json";
+let fullUrl = baseUrlStart + dynamicPartUrl + baseUrlEnd;
+
+// Functions
+const handler = () => {
+  dynamicPartUrl = inputField.value;
+  selectedCity.textContent = inputField.value;
+  console.log(fullUrl);
+};
+
+// Event Listeners for creating the API url request
+inputField.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    handler();
+    getData();
+  }
+});
+
+searchBtn.addEventListener("click", handler);
+
+async function getData() {
+  fetch(fullUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+      // currentTemperature.textContent = response
+    });
+}
