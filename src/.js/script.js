ae012bcf2413a243cedb623cbe47ca384068d391
Michael <100490812+DebSol11@users.noreply.github.com>
2025-12-15 20:26:43 +0400

    Make functions work in the right order

--- src/.js/script.js
+++ src/.js/script.js
@@ -6,6 +6,7 @@ let searchBtn = document.querySelector("#weatherSearchBtn");
 let selectedCity = document.querySelector("#selectedCity");
 let currentDay = document.querySelector("#currentDay");
 let currentTemperature = document.querySelector("#currentTemperature");
+
 let weatherDataObject = {};
 
 // API Url creation variables
@@ -16,54 +17,53 @@ let baseUrlEnd =
   "?unitGroup=us&include=days%2Ccurrent&key=SPQCWP3UPXJYLXTRTVL652LC5&contentType=json";
 let fullUrl;
 
-// Functions
-function createUrl() {
-  dynamicPartUrl = inputField.value;
-  console.log(dynamicPartUrl);
-  fullUrl = baseUrlStart + dynamicPartUrl + baseUrlEnd;
-  console.log(fullUrl);
-}
-
 // Event Listeners for creating the API url request
 inputField.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    createUrl();
-    getData();
-    displayData();
+    executeFunctions();
   }
 });
 
 searchBtn.addEventListener("click", function () {
-  createUrl();
-  getData();
-  displayData();
+  executeFunctions();
 });
+
+// Functions
+function createUrl() {
+  dynamicPartUrl = inputField.value;
+  fullUrl = baseUrlStart + dynamicPartUrl + baseUrlEnd;
+}
 
 async function getData() {
   const response = await fetch(fullUrl);
   const weatherData = await response.json();
-    console.log(weatherData);
-    weatherDataObject = {
-      address: weatherData.address,
-      currentDate: weatherData.days[0].datetime,
-      temperatureCelsiusCurrent:
-        Math.round(
-          generalFunc.convertTempFromFahrenheitToCelsius(
-            weatherData.currentConditions.temp
-          ) * 10
-        ) / 10,
-      tomorrowDate: weatherData.days[1].datetime,
-      temperatureCelsiusTomorrow:
-        Math.round(
-          generalFunc.convertTempFromFahrenheitToCelsius(weatherData.days[1].temp) * 10
-        ) / 10,
-    };
-    console.log(weatherDataObject);
+  weatherDataObject = {
+    address: weatherData.address,
+    currentDate: weatherData.days[0].datetime,
+    temperatureCelsiusCurrent:
+      Math.round(
+        generalFunc.convertTempFromFahrenheitToCelsius(
+          weatherData.currentConditions.temp
+        ) * 10
+      ) / 10,
+    tomorrowDate: weatherData.days[1].datetime,
+    temperatureCelsiusTomorrow:
+      Math.round(
+        generalFunc.convertTempFromFahrenheitToCelsius(
+          weatherData.days[1].temp
+        ) * 10
+      ) / 10,
   };
-
+}
 
 function displayData() {
-  selectedCity.textContent = getData().address;
+  selectedCity.textContent = weatherDataObject.address;
   currentDay.textContent = weatherDataObject.currentDate;
-  // currentTemperature.textContent = ;
+  currentTemperature.textContent = weatherDataObject.temperatureCelsiusCurrent;
+}
+
+async function executeFunctions() {
+  await createUrl();
+  await getData();
+  await displayData();
 }
